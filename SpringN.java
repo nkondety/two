fdf764e0f8692c3e3c1ce2835c7ec1774cee403c
Naren <nkondety@gmail.com>
2016-04-28 20:04:35 -0500

    Added files via upload

---
+++ SpringN.java
@@ -0,0 +1,11 @@
+package com.naren.serv;
+
+public class SpringN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Springn1 sp= new Springn1();
+		sp.naren("naren");
+	}
+
+}
---
+++ Springn1.java
@@ -0,0 +1,14 @@
+package com.naren.serv;
+
+public class Springn1 {
+	//public void naren(String name){
+	//	System.out.println("this is a naren method"+name);
+	//}
+
+	public void naren(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a naren method"+name);
+	}
+
+	
+}
